9f2a01987095dd6b7496bdb64f6da67b0369d867
kiwm <pedro.malta@liferay.com>
2022-09-09 16:58:36 -0300

    airbnb v1

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import airbnb from "./airbnb.png"
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={airbnb}></img>
+        </nav>
+    )
+}
